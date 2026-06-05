5f15e483e45e0e232193597c2fb978d1a9c491ec
agung44234234 <cloud@devdat.ai>
2026-06-06 05:47:37 +0700

    release v2

--- app.js
+++ app.js
@@ -4,17 +4,16 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Data Diri V1</h1>
+        <h1>Data Diri V2</h1>
         <p>Nama: Agung Sudarmanto</p>
-        <p>Role: Cloud & DevOps Engineer</p>
-        <p>Version: 1.1</p>
+        <p>Role: Cloud Engineer</p>
+        <p>Skill: AWS, Azure, Docker, Kubernetes</p>
+        <p>Version: 2.0</p>
     `);
-
-   
 });
-
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
+
 
   
