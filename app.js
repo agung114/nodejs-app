ad171c1c9b223ce682286d3540ff3570ddf59476
agung44234234 <cloud@devdat.ai>
2026-06-09 10:42:16 +0700

    release v3

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send(`
         <h1>Data Diri V2</h1>
-        <p>Nama: Agung Sudarmanto</p>
+        <p>Nama: Agung Sudarmanto 3</p>
         <p>Role: Cloud Engineer</p>
         <p>Skill: AWS, Azure, Docker, Kubernetes</p>
-        <p>Version: 2.0</p>
+        <p>Version: 3.0</p>
     `);
 });
 app.listen(3000, () => {
