e95508e25ed28d97969c2526ef5111bbfc3deed4
agung114 <sudarmantoagung16@gmail.com>
2026-06-17 10:50:12 +0700

    release v10

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Data Diri V5</h1>
+        <h1>Data Diri V50</h1>
         <p>Nama: Agung Sudarmanto </p>
         <p>Role: Cloud Engineer</p>
         <p>Skill: AWS, Azure, Docker, Kubernetes</p>
-        <p>Version: 5.0</p>
+        <p>Version: 50.0</p>
     `);
 });
 app.listen(3000, () => {
-    console.log('release v10');
+    console.log('release v50');
 });
 
 
